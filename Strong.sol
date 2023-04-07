a6182458a57411a2eb2d8cd04c78bc3319ca77ac
wisdomn200 <wisdomn200@gmail.com>
2023-04-07 14:25:53 +0100

    progress

--- Strong.sol
+++ Strong.sol
@@ -1,29 +1,31 @@
 // Strong contract is protected from re-entrancy using transfer function and mutex.
 // I also ensured that all logic that changes state happend before any external call
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
 contract Strong {
     // initialise the mutex
-    bool reEntracyMutex = false;
+    bool reEntrancyMutex = false;
     uint256 public withdrawalLimit = 1 ether;
     mapping(address => uint256) public lastWithdrawTime;
     mapping(address => uint256) public balances;
 
     function depositFunds() public payable {
-        balances[msg.sender] += msg.sender;
+        balances[msg.sender] += msg.value;
     }
 
     function withdrawFunds(uint256 _weiToWithdraw) public {
-        require(!reEntracyMutex);
+        require(!reEntrancyMutex);
         require(balances[msg.sender] >= _weiToWithdraw);
         // limit the withdrwal
         require(_weiToWithdraw <= withdrawalLimit);
         // limit the time allowed to withdraw
-        require(now >= lastWithdrawTime[msg.sender] + 1 weeks);
+        require(block.timestamp >= lastWithdrawTime[msg.sender] + 1 weeks);
         balances[msg.sender] -= _weiToWithdraw;
-        lastWithdrawTime[msg.sender] = now;
+        lastWithdrawTime[msg.sender] = block.timestamp;
         // set the reEntrancy mutex before the external call
         reEntrancyMutex = true;
-        msg.sender.transfer(_weiToWithdraw);
+        payable(msg.sender).transfer(_weiToWithdraw);
         // release the mutex after the external call
         reEntrancyMutex = false;
     }
